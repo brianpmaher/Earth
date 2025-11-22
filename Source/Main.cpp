0a2178aff6763c5a1a2b5a9c1145856214a2c982
Brian Maher <bmaher91@gmail.com>
2025-11-22 14:28:36 -0800

    Use angle brace for included imports

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,10 +1,10 @@
 #include "Logger.hpp"
 
-#include "SDL3/SDL_events.h"
-#include "SDL3/SDL_init.h"
-#include "SDL3/SDL_keycode.h"
-#include "SDL3/SDL_video.h"
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_keycode.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_video.h>
 
 #include <print>
 
